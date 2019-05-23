eeb611ec44368449b3edf836923c4eb265df4112
Oliver <oliver_jonsson93@hotmail.com>
2019-05-23 19:01:32 +0200

    Login transition example

--- BlockbusterProject/src/scene/loginScreen/LoginScreenController.java
+++ BlockbusterProject/src/scene/loginScreen/LoginScreenController.java
@@ -1,5 +1,9 @@
 package scene.loginScreen;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
+import javafx.util.Duration;
 import scene.userMenu.UserMenuController;
 import database.DbConnector;
 import javafx.event.ActionEvent;
@@ -12,6 +16,7 @@ import javafx.scene.input.MouseEvent;
 import model.Logic;
 
 import java.net.URL;
+import java.security.Key;
 import java.util.ResourceBundle;
 
 public class LoginScreenController implements Initializable {
@@ -22,7 +27,7 @@ public class LoginScreenController implements Initializable {
     @FXML
     Button signIn, btnCreateAccount;
     @FXML
-    ImageView imageSwap, logo;
+    ImageView imageSwap, logo,avengersSwap,kingarthurSwap,hpSwap,warcraftSwap,lotrSwap,wpSwap,inceptionSwap,venturaSwap;
 
     private DbConnector dbConnector = new DbConnector();
     private Logic logic = new Logic();
@@ -86,5 +91,33 @@ public class LoginScreenController implements Initializable {
         transition.setAutoReverse(true);
         transition.setCycleCount(PathTransition.INDEFINITE);
         transition.play();*/
+
+
+        KeyFrame kf16 = new KeyFrame(Duration.seconds(2), new KeyValue(venturaSwap.opacityProperty(),1));
+        KeyFrame kf12 = new KeyFrame(Duration.seconds(3),new KeyValue(venturaSwap.opacityProperty(),0));
+        KeyFrame kf13 = new KeyFrame(Duration.seconds(5),new KeyValue(inceptionSwap.opacityProperty(),1));
+        KeyFrame kf10 = new KeyFrame(Duration.seconds(7),new KeyValue(inceptionSwap.opacityProperty(),0));
+
+        KeyFrame kf11 = new KeyFrame(Duration.seconds(9),new KeyValue(wpSwap.opacityProperty(),1));
+        KeyFrame kf8 = new KeyFrame(Duration.seconds(11),new KeyValue(wpSwap.opacityProperty(),0));
+
+        KeyFrame kf9 = new KeyFrame(Duration.seconds(13),new KeyValue(lotrSwap.opacityProperty(),1));
+        KeyFrame kf6 = new KeyFrame(Duration.seconds(15), new KeyValue(lotrSwap.opacityProperty(),0));
+
+        KeyFrame kf5 = new KeyFrame(Duration.seconds(17), new KeyValue(warcraftSwap.opacityProperty(),1));
+        KeyFrame kf7 = new KeyFrame(Duration.seconds(19),new KeyValue(warcraftSwap.opacityProperty(),0));
+
+        KeyFrame kf2 = new KeyFrame(Duration.seconds(21),new KeyValue(hpSwap.opacityProperty(),1));
+        KeyFrame kf1 = new KeyFrame(Duration.seconds(23),new KeyValue(hpSwap.opacityProperty(),0));
+
+        KeyFrame kf3 = new KeyFrame(Duration.seconds(25),new KeyValue(kingarthurSwap.opacityProperty(),1));
+        KeyFrame kf4 = new KeyFrame(Duration.seconds(27),new KeyValue(kingarthurSwap.opacityProperty(),0));
+        KeyFrame kf15 = new KeyFrame(Duration.seconds(29),new KeyValue(avengersSwap.opacityProperty(),1));
+
+        Timeline timelineOn = new Timeline(kf12,kf13,kf10,kf11,kf8,kf9, kf6, kf5, kf7, kf2,kf1, kf3 ,kf4,kf16,kf15);
+        timelineOn.setCycleCount(Timeline.INDEFINITE);
+        timelineOn.setAutoReverse(true);
+        timelineOn.play();
+
     }
 }
